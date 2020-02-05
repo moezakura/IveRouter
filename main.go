ca3f1bc7a68f18cec5d344272dce8a5c0ac1fb0f
mox <mox+git@mox.si>
2020-02-05 23:50:11 +0900

    日毎に統計をリセットするようにした

--- main.go
+++ main.go
@@ -23,15 +23,18 @@ func main() {
 	as := module.NewAutoSave(pl)
 	as.Run()
 
+	drs := module.NewDailyReset(pl)
+	drs.Run()
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		var devices model.Devices = pl.GetDevices()
 		sort.Sort(sort.Reverse(devices))
 		fmt.Println("==== DEVICE TRAFFIC ====")
 
-		fmt.Printf("%17s : ", "MAC ADDRESS")
-		fmt.Printf("%13s%s%13s   ", "", "UPLOAD", "")
-		fmt.Printf("%12s%s%12s", "", "DOWNLOAD", "")
+		fmt.Printf("%13s%s%13s  ", "", "UPLOAD", "")
+		fmt.Printf(" : %3s%s%3s : ", "", "MAC ADDRESS", "")
+		fmt.Printf("  %12s%s%12s", "", "DOWNLOAD", "")
 		fmt.Println()
 
 		for _, d := range devices {
@@ -39,9 +42,10 @@ func main() {
 			downloadTraffic := d.DownloadTraffic
 			upload := util.ToDataCast(float64(uploadTraffic))
 			download := util.ToDataCast(float64(downloadTraffic))
-			fmt.Printf("%s : ", d.MacAddress)
 			fmt.Printf("↑ %10s (% 15d b)", upload, uploadTraffic)
-			fmt.Printf("   ")
+			fmt.Printf("  ")
+			fmt.Printf(" : %s : ", d.MacAddress)
+			fmt.Printf("  ")
 			fmt.Printf("↓ %10s (% 15d b)", download, downloadTraffic)
 			fmt.Printf("\n")
 		}
--- module/daily_reset.go
+++ module/daily_reset.go
@@ -1,1 +1,36 @@
 package module
+
+import (
+	"sync"
+	"time"
+)
+
+type DailyReset struct {
+	packetLive *PacketLive
+
+	lastDate string
+}
+
+func NewDailyReset(packetLive *PacketLive) *DailyReset {
+	return &DailyReset{packetLive: packetLive}
+}
+
+func (d *DailyReset) Run() {
+	go func() {
+		t := time.NewTicker(50 * time.Millisecond)
+		for {
+			<-t.C
+			d.do()
+		}
+	}()
+}
+
+func (d *DailyReset) do() {
+	now := time.Now()
+	today := now.Format("2006-01-02")
+	if d.lastDate == today {
+		return
+	}
+	d.lastDate = today
+	d.packetLive.devices = sync.Map{}
+}
